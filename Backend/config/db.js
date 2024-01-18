9c2a1b5006c3f2c708342defdfd229deabf50895
Shubhanshu Shukla <shubhanshu.shukla_cs.aiml19@gla.ac.in>
2024-01-18 22:10:49 +0530

    few changes

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,12 +1,16 @@
-import mongoose from 'mongoose'
-import colors from 'colors'
-const connectDB= async()=>{
-  try{
-    const conn=await mongoose.connect(process.env.MONGO_URL)
-    console.log(`Connected to MongoDB Database ${conn.connection.host}`.bgGreen.white)
-  }catch(error){
-    console.log(`Error in MongoDB ${error}`.bgRed.white)
+import mongoose from "mongoose";
+import colors from "colors";
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(
+      "mongodb+srv://shubhanshushukla1975:hppZOvbWvefJ3Tnc@ecommerce.hdutin9.mongodb.net/ecommerce"
+    );
+    console.log(
+      `Connected to MongoDB Database ${conn.connection.host}`.bgGreen.white
+    );
+  } catch (error) {
+    console.log(`Error in MongoDB ${error}`.bgRed.white);
   }
-}
+};
 
 export default connectDB;
--- Backend/middlewares/authMiddleware.js
+++ Backend/middlewares/authMiddleware.js
@@ -7,7 +7,7 @@ export const requireSignIn = async (req, res, next) => {
   try {
     const decode = JWT.verify(
       req.headers.authorization,
-      process.env.JWT_SECRET
+      BJSJCBJSDCIKCSBY82YH8Y237RBX
     );
     req.user = decode;
     next();
